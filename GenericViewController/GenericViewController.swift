481df52ef999b2416c8bdfff9506f375bfe56a77
Dean Silfen <dean.silfen@gmail.com>
2018-04-25 13:36:22 -0400

    Update to only have a render method

--- GenericViewController/GenericViewController.swift
+++ GenericViewController/GenericViewController.swift
@@ -9,36 +9,16 @@
 import UIKit
 
 open class GenericViewController<ModelType>: UIViewController {
-  open var model: ModelType? {
-    didSet {
-      self.render()
+    open var model: ModelType? {
+        didSet {
+            self.render()
+        }
     }
-  }
 
-  override open func viewDidLoad() {
-      super.viewDidLoad()
-      self.render()
-  }
-
-  // set up your UI here instead
-  open func render() {
-    
-  }
-
-  override open func didReceiveMemoryWarning() {
-      super.didReceiveMemoryWarning()
-      // Dispose of any resources that can be recreated.
-  }
-  
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    override open func viewDidLoad() {
+        super.viewDidLoad()
+        self.render()
     }
-    */
 
+    open func render() { }
 }
