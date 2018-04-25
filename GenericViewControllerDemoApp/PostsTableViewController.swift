69098dfa5e02ea5120590ff843a6f87def8e5110
Dean Silfen <dean.silfen@gmail.com>
2018-04-25 13:40:17 -0400

    Get demo tableview working

--- GenericViewControllerDemoApp/PostsTableViewController.swift
+++ GenericViewControllerDemoApp/PostsTableViewController.swift
@@ -42,7 +42,7 @@ class PostsTableViewController: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
