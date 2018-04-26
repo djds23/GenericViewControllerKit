ae63ec1f8da5794c9c2a2ef4dea3d10e2d4c30db
Dean Silfen <dean.silfen@gmail.com>
2018-04-26 09:40:36 -0400

    Remove build folder

--- build/GenericViewControllerKit.build/Release-iphoneos/GenericViewControllerKit.build/Objects-normal/arm64/GenericViewControllerKit_vers.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/deansilfen/Projects/GenericViewControllerKit/build/GenericViewControllerKit.build/Release-iphoneos/GenericViewControllerKit.build/DerivedSources/GenericViewControllerKit_vers.c
