739dfdb58844f74fd6ab1f86dedcc338f7b1d848
Dean Silfen <dean.silfen@gmail.com>
2018-04-26 09:28:15 -0400

    GenericViewController doc update

--- GenericViewControllerKit/GenericViewController.swift
+++ GenericViewControllerKit/GenericViewController.swift
@@ -8,7 +8,13 @@
 
 import UIKit
 
+/// A subclass of UIViewController that leverages swift
+/// generics to have a type safe model-view relationship.
 open class GenericViewController<ModelType>: UIViewController {
+
+    /// `model` a generic property that should
+    /// contain data needed for the `render`
+    /// function to bind data to the view.
     open var model: ModelType? {
         didSet {
             self.render()
@@ -20,6 +26,10 @@ open class GenericViewController<ModelType>: UIViewController {
         self.render()
     }
 
+    /// `render()` should be overriden and code that binds
+    /// the UI to the new data should be placed here.
+    /// This method will be called when the view loads,
+    /// and every subseqeunt time when a new model is set.
     open func render() {
         // #warning Incomplete implementation, override to update your view when the model changes
     }
